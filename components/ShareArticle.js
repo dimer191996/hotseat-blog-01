0cc3b63134eba80adaccdcc6b7992af8e6df0fae
dimer191996 <db191996@gmail.com>
2022-05-08 09:42:50 +0200

    fix share article button link

--- components/ShareArticle.js
+++ components/ShareArticle.js
@@ -39,49 +39,49 @@ export default function ShareArticle({ slug }) {
       >
         <div className="flex flex-col  space-y-4 ">
           <FacebookMessengerShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <FacebookMessengerIcon size={28} round={true} />
           </FacebookMessengerShareButton>
 
           <FacebookShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <FacebookIcon size={28} round={true} />
           </FacebookShareButton>
 
           <TwitterShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <TwitterIcon size={28} round={true} />
           </TwitterShareButton>
 
           <EmailShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <EmailIcon size={28} round={true} />
           </EmailShareButton>
 
           <WhatsappShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <WhatsappIcon size={28} round={true} />
           </WhatsappShareButton>
 
           <TelegramShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <TelegramIcon size={28} round={true} />
           </TelegramShareButton>
 
           <MailruShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <MailruIcon size={28} round={true} />
           </MailruShareButton>
 
           <WorkplaceShareButton
-            url={`${"https://hilarious-druid-15a4a5.netlify.app/article/"}${slug}`}
+            url={`${"https://www.hotseatmag.com/article/"}${slug}`}
           >
             <WorkplaceIcon size={28} round={true} />
           </WorkplaceShareButton>
