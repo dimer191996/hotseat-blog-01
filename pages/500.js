5864e0926cef1d6df5d657785fedcb511cf52c03
dimer191996 <db191996@gmail.com>
2022-05-28 04:57:15 +0200

    server error

--- pages/500.js
+++ pages/500.js
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
 export default function Custom500() {
-  return <h1 className=" text-center"></h1>;
+  return (
+    <div className=" flex justify-center text-center">
+      <h1 className=" lg:text-3xl text-2xl font-black">
+        We apologize for the inconvinience{" "}
+      </h1>
+      <div className=" text-xl font caret-black">
+        {" "}
+        The website is currently under mentenance
+      </div>
+      <Link href={"/subscription"}>
+        <div className=" font-bold to-blue-600">
+          Click here | Take This Step Unlock
+        </div>
+      </Link>
+    </div>
+  );
 }
