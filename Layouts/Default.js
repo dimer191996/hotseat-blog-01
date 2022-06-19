2d23c005e2b68369e68af542cb84fb1fed860a98
dimer191996 <db191996@gmail.com>
2022-06-19 14:10:30 +0200

    update sitemap

--- Layouts/Default.js
+++ Layouts/Default.js
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 export default function Default({ children }) {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
   useEffect(() => {
     if (window.location.search.includes("success=true")) {
       setVisible(false);
